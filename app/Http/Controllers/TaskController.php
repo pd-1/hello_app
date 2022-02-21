197f7014e780b738aae833aff62bdce9604c90d0
DAIGA <dayaxiqi@gmail.com>
2022-02-21 23:36:21 +0900

    tasks index

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(int $id)
     {
-        $folders = Folder::all();
+    $folders = Folder::all();
 
-        return view('tasks/index',[
-            'folders' => $folders,
-        ]);
+    $current_folder = Folder::find($id);
+
+    $tasks = $current_folder->tasks()->get();
+
+    return view('tasks/index', [
+        'folders' => $folders,
+        'current_folder_id' => $current_folder->id,
+        'tasks' => $tasks,
+    ]);
     }
 }
