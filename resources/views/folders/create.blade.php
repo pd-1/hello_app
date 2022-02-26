a73c23763809c98274fd0ca5a208d2ce7df5551e
DAIGA <dayaxiqi@gmail.com>
2022-02-26 09:26:58 +0900

    createの作成

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
   <header>
@@ -20,11 +20,19 @@
           <nav class="panel panel-default">
             <div class="panel-heading">フォルダを追加する</div>
             <div class="panel-body">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+            <ul>@foreach($errors->all() as $message)
+              <li>{{ $message }}</li>
+              @endforeach
+            </ul>
+            </div>
+            @endif
               <form action="{{ route('folders.create') }}" method="post">
                 @csrf
                 <div class="form-group">
                   <label for="title">フォルダ名</label>
-                  <input type="text" class="form-control" name="title" id="title" />
+                  <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
                 </div>
                 <div class="text-right">
                   <button type="submit" class="btn btn-primary">送信</button>
