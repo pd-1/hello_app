18437d7b0fae32f2929eeda9c44d63d238497fee
DAIGA <dayaxiqi@gmail.com>
2022-03-10 09:42:15 +0900

    create作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -21,5 +22,36 @@ class TaskController extends Controller
         'current_folder_id' => $current_folder->id,
         'tasks' => $tasks,
     ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id,
+        ]);
+    }
+
+    public function create(int $id,CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index',[
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit' , [
+            'task' => $task,
+        ]);
     }
 }
