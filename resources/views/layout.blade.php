9d67731c1b01077ae6c21cd1921321991bc6a9fb
DAIGA <dayaxiqi@gmail.com>
2022-03-19 23:22:09 +0900

    ログイン機能

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,11 +12,26 @@
 <header>
   <nav class="my-navbar">
     <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
+    <div class="my-navbar-control">
+    @if(Auth:check())
+    <span class="my-navbar-item">ようこそ,{{ Auth::user()->name }}さん</span>
+    |
+    <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+    <from id="logout-form" action="{{ route('logout }}" method="POST" style="display: none;">
+    @csrf
+    </from>
+    <a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+        ｜
+        <a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+      @endif
+    </div>
+    </nav>
 </header>
 <main>
   @yield('content')
 </main>
+@if(Auth::check())
 @yield('scripts')
+@endif
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => 'auth'], function() {
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
@@ -17,6 +18,6 @@ Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name(
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
-
 Route::get('/','HomeController@index')->name('home');
+})
 Auth::routes();
